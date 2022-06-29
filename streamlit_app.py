75c9e605bb25b0837c3af528ab1f96490c79c51d
bulattoolpix <b.n.kamaev@gmail.com>
2022-06-29 13:06:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -582,16 +582,3 @@ for variable in dir():
         del globals()[variable]
 del variable
     
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
