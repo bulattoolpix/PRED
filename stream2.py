e59a5082f8109f71a92abd180821ac0abe72a607
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-23 22:33:46 +0300

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -22,7 +22,7 @@ Upload your experiment results to see the significance of your A/B test.
 uploaded_file = st.file_uploader(
         "",
         key="1",
-        help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
+     
     )
 
 if uploaded_file is not None:
