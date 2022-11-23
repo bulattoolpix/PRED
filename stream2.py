cfa8c3fa92649745496a5ca7030177e591526962
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-23 21:54:34 +0300

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -57,21 +57,6 @@ classifier.fit(X_train, y_train)
 
 
 
-def to_excel(df):
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
-    workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
-    format1 = workbook.add_format({'num_format': '0.00'}) 
-    worksheet.set_column('A:A', None, format1)  
-    writer.save()
-    processed_data = output.getvalue()
-    return processed_data
-df_xlsx = to_excel(df)
-
-def welcome():
-    return 'welcome all'
   
     
     
