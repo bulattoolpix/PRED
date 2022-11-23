3e90afa2da9822aecc1bf0ed3bc407c031dfdf5d
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-23 18:22:13 +0300

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -99,6 +99,19 @@ def main():
      
 if __name__=='__main__':
     main()
+    
+  def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+csv = convert_df(df)
+
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='large_df.csv',
+    mime='text/csv',
+)
 
     
 
