9fd6e579cfb59528ce13d38d5ece07e5de150156
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-23 16:20:08 +0300

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -9,14 +9,31 @@ from io import StringIO
 
 
 
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    ##st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    ##st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
 
 
 
 ##raw import data 2019
 url19 = "https://raw.githubusercontent.com/bulattoolpix/streamlit-example/master/Iris.csv" 
 download19 = requests.get(url19).content
-df= pd.read_csv(io.StringIO(download19.decode('utf-8')))
-
+##df= pd.read_csv(io.StringIO(download19.decode('utf-8')))
+df=dataframe
 df.drop('Id', axis = 1, inplace = True)
 # Renaming the target column into numbers to aid training of the model
 df['Species']= df['Species'].map({'Iris-setosa':0, 'Iris-versicolor':1, 'Iris-virginica':2})
@@ -85,22 +102,5 @@ if __name__=='__main__':
     
     
     
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    ##st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    ##st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
 
 
