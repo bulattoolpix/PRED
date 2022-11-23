f65b24036fd64cd4645e6722ddf9763357b4de82
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-23 17:11:08 +0300

    Create loader.py

---
+++ loader.py
@@ -0,0 +1,30 @@
+
+st.write(
+    """
+# 📊 A/B Testing App
+Upload your experiment results to see the significance of your A/B test.
+"""
+)
+
+uploaded_file = st.file_uploader("Upload CSV", type=".csv")
+
+use_example_file = st.checkbox(
+    "Use example file", False, help="Use in-built example file to demo the app"
+)
+
+ab_default = None
+result_default = None
+
+# If CSV is not uploaded and checkbox is filled, use values from the example file
+# and pass them down to the next if block
+if use_example_file:
+    uploaded_file = "Website_Results.csv"
+    ab_default = ["variant"]
+    result_default = ["converted"]
+
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+
+    st.markdown("### Data preview")
+    st.dataframe(df.head())
