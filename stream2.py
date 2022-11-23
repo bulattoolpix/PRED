208785f5b89e784d3d30a2a9ee97e236a145d0c5
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-23 22:26:57 +0300

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -18,24 +18,24 @@ Upload your experiment results to see the significance of your A/B test.
 """
 )
 
-  
+    
 uploaded_file = st.file_uploader("Choose a file")
-
 if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    # To read file as bytes:
+    ##bytes_data = uploaded_file.getvalue()
+    ##st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    ##st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    ##st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
-        df= pd.read_csv(uploaded_file)
-        st.write(df)   
-else:
-        st.info(
-       
-        )
-
-        st.stop()
-  
-
+     df= pd.read_csv(uploaded_file)
+     st.write(df)   
 
    
 df.drop('Id', axis = 1, inplace = True)
