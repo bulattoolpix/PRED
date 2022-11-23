800c9e60157dd89a0318848be6d92e3476957f70
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-23 21:41:00 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -5,33 +5,17 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
 @st.cache
-def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('utf-8')
+ def convert_df(df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return df.to_csv().encode('utf-8')
 
+csv = convert_df(my_large_df)
 
-st.write(
-    """
-# 📊 A/B Testing App
-Upload your experiment results to see the significance of your A/B test.
-"""
-)
-
-uploaded_file = st.file_uploader("Upload CSV", type=".csv")
-
-
-ab_default = None
-result_default = None
-
-if uploaded_file:
-    df = pd.read_csv(uploaded_file)
-    st.markdown("### Data preview")
-    st.dataframe(df.head())
-    csv = convert_df(df)
-
- st.download_button(
-    label="Download data as CSV",
-    data=csv,
-    file_name='large_df.csv',
-    mime='text/csv')
+st.download_button(
+     label="Download data as CSV",
+     data=csv,
+     file_name='large_df.csv',
+     mime='text/csv',
+ )
