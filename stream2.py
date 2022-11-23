f3cd6982a888a10172da8e77ffafceeeb4babeb2
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-23 16:00:08 +0300

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -84,21 +84,22 @@ if __name__=='__main__':
     
     
     
-    
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-# To read file as bytes:
-bytes_data = uploaded_file.getvalue()
-st.write(bytes_data)
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
 
     # To convert to a string based IO:
-stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-st.write(stringio)
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
 
     # To read file as string:
-string_data = stringio.read()
-st.write(string_data)
+    string_data = stringio.read()
+    st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
-dataframe = pd.read_csv(uploaded_file)
-st.write(dataframe)
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
+
