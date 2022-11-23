6d6475134de75371f86ed863c927493d89da966f
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-23 22:24:23 +0300

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -23,8 +23,11 @@ uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
-        uploaded_file.seek(0)
-        file_container.write(df)
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+
+    # Can be used wherever a "file-like" object is accepted:
+        df= pd.read_csv(uploaded_file)
+        st.write(df)   
 else:
         st.info(
        
