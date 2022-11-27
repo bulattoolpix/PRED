384f30625f20f4c0f5e47e01ac14183c35635289
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-27 17:58:39 +0300

    Update mllit.py

--- mllit.py
+++ mllit.py
@@ -346,9 +346,9 @@ def main():
               feature_importance=df_feature.sort_values(by='Feature Importance', ascending=False).T
               feature_importance
               dfx1
-              ##sorted_idx = pd.DataFrame(model_xgb3.feature_importances_)
+              sorted_idx = pd.DataFrame(model_xgb3.feature_importances_,columns=list(dfx1.columns))
               ##sorted_idx .columns =dfx1.columns
-              ##st.bar_chart( sorted_idx)
+              st.bar_chart( sorted_idx)
 
               ##st.bar_chart(model_xgb3.feature_importances_.rename_axis('unique_values'), label=data.columns)
 
