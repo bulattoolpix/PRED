6894cf383dc50052361ae7c1764cdaafb4752170
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-27 18:10:02 +0300

    Update mllit.py

--- mllit.py
+++ mllit.py
@@ -348,7 +348,7 @@ def main():
               dfx1.columns
               list(dfx1)
               dfx1.columns.to_array
-              sorted_idx = pd.DataFrame(model_xgb3.feature_importances_,columns=list(dfx1.columns))
+              sorted_idx = pd.DataFrame(model_xgb3.feature_importances_,columns= dfx1.columns.values.tolist())
            
               ##sorted_idx .columns =dfx1.columns
               st.bar_chart( sorted_idx)
