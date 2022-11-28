b19f6b04d82c60b09fdc09dd5920129e0e47cf15
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-28 21:54:02 +0300

    Update mllit.py

--- mllit.py
+++ mllit.py
@@ -312,7 +312,7 @@ def main():
 
   
     uploaded_file2 = st.file_uploader("Choose a CSV file_topredict", type="csv")
-    data2 = pd.read_csv(uploaded_file2, low_memory=False)
+    data2 = pd.read_csv(uploaded_file2)
     st.write('Uploaded data:', data2.head(30))
     scaler = MinMaxScaler() 
     V = scaler.fit_transform( data2 )
