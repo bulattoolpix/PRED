f51244d75bd7c6399df2cacd25187dc163cc6348
bulattoolpix <b.n.kamaev@gmail.com>
2022-11-29 13:56:51 +0300

    Update mllit.py

--- mllit.py
+++ mllit.py
@@ -1,3 +1,4 @@
+@ -1,361 +1,357 @@
 import streamlit as st 
 import streamlit.components.v1 as stc 
 import pandas as pd
@@ -145,7 +146,7 @@ def home_page_builder(df, data, rows, columns):
 
    
     # Insert Check-Box to show the snippet of the data.
-    if st.checkbox('Show Data'):
+    if  st.checkbox('Show Data'):
         st.subheader("Raw data")
         st.write(
             f'Input dataset includes **{rows}** rows and **{columns}** columns')
@@ -292,41 +293,52 @@ def xgb_page_builder(data,data2 ):
 def main():
     """Streamlit demo web app"""
     
+    st.write(
+    """
+# 📊 AUTO CLASSIFIER App
+Загрузите файл для обучения и файл для прогноза 
+"""
+)
     uploaded_file = st.file_uploader(
         "",
         key="1",
      
     )
-
     if uploaded_file is not None:
       
         df = pd.read_csv(uploaded_file)
         uploaded_file.seek(0)
     
-    df, data, filename, rows, columns = upload_different_data(uploaded_file)
+        df, data, filename, rows, columns = upload_different_data(uploaded_file)
+        home_page_builder(df, data, rows, columns)
+
+    
+       
     
 
 
+  
     uploaded_file2 = st.file_uploader("Choose a CSV file_topredict", type="csv")
     data2 = pd.read_csv(uploaded_file2, low_memory=False)
     st.write('Uploaded data:', data2.head(30))
     scaler = MinMaxScaler() 
     V = scaler.fit_transform( data2 )
+   
             
             
 
     st.sidebar.title('Menu')
     choose_model = st.sidebar.selectbox("Choose the page or model", [
-                                        "Home",  "XGB"])
-    
-    
+                                        "Home",  "XGB"])    
     
     
     if choose_model == "Home":
-        home_page_builder(df, data, rows, columns)
+       home_page_builder(df, data, rows, columns)
+       
+
     if choose_model == "XGB":
         model_xgb = xgb_page_builder(data,data2  )
-        if(st.checkbox("Want to Use this model to predict on a new dataset?")):
+        if(st.checkbox("Want to check Feature importance")):
            ##prediction_downloader(data2) ###загрузк
  ##             featureimp (df)
               scaler = MinMaxScaler()  
@@ -342,27 +354,10 @@ def main():
               df_feature = pd.DataFrame.from_dict(model_xgb3.get_booster().get_fscore(), orient='index')
              ## df_feature.columns =dfx1.columns.values.tolist()
               df_feature.columns = ['Feature Importance']
-              df_feature.columns =dfx1.columns
+              ##df_feature.columns =dfx1.columns
               list(dfx1.columns)
               df_feature
               
 
 #Using list(df) to get the list of all Column Names
 
-             ## st.bar_chart(df_feature)
-    
-              feature_importance=df_feature.sort_values(by='Feature Importance', ascending=False).T
-              feature_importance
-              dfx1.columns
-              list(dfx1)
-              dfx1.columns.values.tolist()
-              
-              sorted_idx = pd.DataFrame(model_xgb3.feature_importances_)
-           
-              ##sorted_idx .columns =dfx1.columns
-              st.bar_chart( sorted_idx)
-
-              ##st.bar_chart(model_xgb3.feature_importances_.rename_axis('unique_values'), label=data.columns)
-
-
-        
